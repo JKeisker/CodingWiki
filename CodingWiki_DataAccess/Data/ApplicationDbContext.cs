e4373269f330a439a1f6cc0076d5da14a19f0874
Joe Keisker <jkeisker@protonmail.com>
2026-03-25 11:03:55 -0500

    Section 2 Migrations - chptrs 11 & 12

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -5,11 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CodingWiki_DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source = HALUCIN8; Database = CodingWiki; Integrated Security = True; Connect Timeout = 30; Encrypt = True; Trust Server Certificate = True; Application Intent = ReadWrite; Multi Subnet Failover = False");
+        }
     }
 }
--- CodingWiki_Model/Models/Book.cs
+++ CodingWiki_Model/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,10 @@ namespace CodingWiki_Model.Models
 {
     public class Book
     {
-        public int IDBook { get; set; }
-        public string Title { get; set; }
-        public string ISBN {  get; set; }
+        [Key]
+        public int BookId { get; set; }
+        public required string Title { get; set; }
+        public required string ISBN {  get; set; }
         public double Price { get; set; }
     }
 }
