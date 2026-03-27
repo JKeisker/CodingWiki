87a0c54ec4ff16c4715a3d6d1136e9509ab99bfe
Joe Keisker <jkeisker@protonmail.com>
2026-03-26 21:14:14 -0500

    Migrations - chptr 25 & 26

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -22,6 +22,19 @@ namespace CodingWiki_DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "Spider Without Duty", ISBN = "123B12", Price = 10.99m },
+                     new Book { BookId = 2, Title = "Fortune of Time", ISBN = "12123B12", Price = 11.99m }
+            );
+
+            var bookList = new Book[]
+            {
+                new Book { BookId = 3, Title = "Fake Sunda", ISBN = "77652", Price = 20.99m },
+                new Book { BookId = 4, Title = "Cookie Jar", ISBN = "CC12B12", Price = 25.99m },
+                new Book { BookId = 5, Title = "Cloudy Forest", ISBN = "90392B33", Price = 40.99m }
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
         }
     }
 }
