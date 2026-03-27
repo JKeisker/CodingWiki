b68a842e5f1b252931d91e318b65098ea097961e
Joe Keisker <jkeisker@protonmail.com>
2026-03-27 09:28:01 -0500

    Data Annotations - chpt 27

--- CodingWiki_Model/Models/Genre.cs
+++ CodingWiki_Model/Models/Genre.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CodingWiki_Model.Models
 {
+    [Table("tb_genres")]
     public class Genre
     {
         public int GenreId { get; set; }
+        [Column("Name")]
         public required string GenreName { get; set; }
         public int DisplayOrder {  get; set; }
     }
