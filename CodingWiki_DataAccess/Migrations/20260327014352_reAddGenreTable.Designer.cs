b9c82d382e376730eb2e938516644d197beae5ca
Joe Keisker <jkeisker@protonmail.com>
2026-03-26 20:51:53 -0500

    Migrations: chptr 23-24

--- CodingWiki_DataAccess/Migrations/20260327014352_reAddGenreTable.Designer.cs
+++ CodingWiki_DataAccess/Migrations/20260327014352_reAddGenreTable.Designer.cs
@@ -3,6 +3,7 @@ using CodingWiki_DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodingWiki_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260327014352_reAddGenreTable")]
+    partial class reAddGenreTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -41,7 +44,7 @@ namespace CodingWiki_DataAccess.Migrations
 
                     b.HasKey("BookId");
 
-                    b.ToTable("Books", (string)null);
+                    b.ToTable("Books");
                 });
 
             modelBuilder.Entity("CodingWiki_Model.Models.Genre", b =>
@@ -60,7 +63,7 @@ namespace CodingWiki_DataAccess.Migrations
 
                     b.HasKey("GenreId");
 
-                    b.ToTable("Genres", (string)null);
+                    b.ToTable("Genres");
                 });
 #pragma warning restore 612, 618
         }
