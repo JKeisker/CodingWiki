06a7e65196351ed29ba79a1d667c20de02e8a2da
Joe Keisker <jkeisker@protonmail.com>
2026-03-28 15:35:07 -0500

    Fluent API: chpt 48

--- CodingWiki_Model/Models/FluentModels/Fluent_Book.cs
+++ CodingWiki_Model/Models/FluentModels/Fluent_Book.cs
@@ -20,7 +20,7 @@ namespace CodingWiki_Model.Models
         //[NotMapped]
         public string PriceRange { get; set; }
 
-        //public Fluent_BookDetail BookDetail { get; set; }
+        public Fluent_BookDetail BookDetail { get; set; }
         //[ForeignKey("Publisher")]
         //public int Publisher_Id { get; set; }
         //public Fluent_Publisher Publisher { get; set; }
