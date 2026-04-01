a0579f09b9313ecd6b1f14454f88c6e1b98b0f67
Joe Keisker <jkeisker@protonmail.com>
2026-04-01 17:58:57 -0500

    Book Mngmt: chps 84 & 85

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -1,6 +1,9 @@
 ﻿using CodingWiki_DataAccess.Data;
 using CodingWiki_Model.Models;
+using CodingWiki_Model.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace CodingWiki_Web.Controllers
 {
@@ -19,22 +22,28 @@ namespace CodingWiki_Web.Controllers
             return View(objList);
         }
 
-        //public IActionResult Upsert(int? id)
-        //{
-        //    Book obj = new();
-        //    if(id ==null || id==0)
-        //    {
-        //        //create
-        //        return View(obj);
-        //    }
-        //    //update
-        //    obj = _db.Books.FirstOrDefault(u => u.BookId == id);
-        //    if(obj == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(obj);
-        //}
+        public IActionResult Upsert(int? id)
+        {
+            BookVM obj = new();
+            obj.PublisherList = _db.Publishers.Select(i => new SelectListItem 
+            { 
+                Text = i.Name,
+                Value = i.Publisher_Id.ToString()
+            });
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(obj);
+            }
+            //update
+            obj.Book = _db.Books.FirstOrDefault(u => u.BookId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
 
         //[HttpPost]
         //[ValidateAntiForgeryToken]
