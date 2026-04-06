26b6da6ab6a6ba986d82d680b17c94368ce3ee20
Joe Keisker <jkeisker@protonmail.com>
2026-04-06 13:35:11 -0500

    Book Mgmt: chps 94-97

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -110,6 +110,58 @@ namespace CodingWiki_Web.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult ManageAuthors(int id)
+        {
+            BookAuthorVM obj = new()
+            {
+                BookAuthorList = _db.bookAuthorMaps.Include(u => u.Author).Include(u => u.Book)
+                    .Where(u => u.Book_Id == id).ToList(),
+                BookAuthor = new()
+                {
+                    Book_Id = id
+                },
+                Book = _db.Books.FirstOrDefault(u => u.BookId == id)
+            };
+
+            List<int> tempListOfAssignedAuthor = obj.BookAuthorList.Select(u=>u.Author_Id).ToList();
+
+            //NOT IN clause
+            //get all the authors where id is not in tempListOfAssignedAuthors
+
+            var tempList = _db.Authors.Where(u => !tempListOfAssignedAuthor.Contains(u.Author_Id)).ToList();
+            obj.AuthorList = tempList.Select(i => new SelectListItem
+            {
+                Text = i.FullName,
+                Value = i.Author_Id.ToString()
+            });
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult ManageAuthors(BookAuthorVM bookAuthorVM)
+        {
+            if (bookAuthorVM.BookAuthor.Book_Id != 0 && bookAuthorVM.BookAuthor.Author_Id != 0)
+            {
+                _db.bookAuthorMaps.Add(bookAuthorVM.BookAuthor);
+                _db.SaveChanges();
+            }
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookAuthorVM.BookAuthor.Book_Id });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveAuthors(int authorId, BookAuthorVM bookAuthorVM)
+        {
+            int bookId = bookAuthorVM.Book.BookId;
+            BookAuthorMap bookAuthorMap = _db.bookAuthorMaps.FirstOrDefault(
+                u => u.Author_Id == authorId && u.Book_Id == bookId);
+
+            _db.bookAuthorMaps.Remove(bookAuthorMap);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookId });
+        }
+
         async public Task<IActionResult> Delete(int id)
         {
             Book obj = new();
