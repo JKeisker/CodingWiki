555eb7dad55197790b9d2f6979197e7f889853d2
Joe Keisker <jkeisker@protonmail.com>
2026-04-07 13:05:20 -0500

    Book Author: chp 99

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -20,21 +20,23 @@ namespace CodingWiki_Web.Controllers
 
         public IActionResult Index()
         {
-            //List<Book> objList = _db.Books.Include(u=>u.Publisher).ToList();
-            List<Book> objList = _db.Books.ToList();
-            foreach (var obj in objList)
-            {
-                //    //least efficient
-                //    //obj.Publisher = _db.Publishers.Find(obj.Publisher_Id);
+            //eager loading
+            List<Book> objList = _db.Books.Include(u => u.Publisher)
+                .Include(u=>u.BookAuthorMap).ThenInclude(u=>u.Author).ToList();
+            //List<Book> objList = _db.Books.ToList();
+            //foreach (var obj in objList)
+            //{
+            //    //    //least efficient
+            //    //    //obj.Publisher = _db.Publishers.Find(obj.Publisher_Id);
 
-                //    //more efficient
-                _db.Entry(obj).Reference(u => u.Publisher).Load();
-                _db.Entry(obj).Collection(u => u.BookAuthorMap).Load();
-                foreach (var bookAuth in obj.BookAuthorMap)
-                {
-                    _db.Entry(bookAuth).Reference(u => u.Author).Load();
-                }
-            }
+            //    //    //more efficient: explicit loading > n+1 problem w/ many hit to db from many queries
+            //    _db.Entry(obj).Reference(u => u.Publisher).Load();
+            //    _db.Entry(obj).Collection(u => u.BookAuthorMap).Load();
+            //    foreach (var bookAuth in obj.BookAuthorMap)
+            //    {
+            //        _db.Entry(bookAuth).Reference(u => u.Author).Load();
+            //    }
+            //}
             return View(objList);
         }
 
